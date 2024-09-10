0116ed8655c37f14eff268d53622049cac4c550c
daniel-kav <dankavatha01@gmail.com>
2024-09-10 17:20:39 +0300

    created visit models

--- src/visits/models.py
+++ src/visits/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Visit(models.Model):
+    path = models.TextField(null=True, blank=True)
+    timestamp = models.DateField(auto_now=True)
