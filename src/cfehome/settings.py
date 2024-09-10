2f641ef4921eb669e294464b870f270e02f5be1c
daniel-kav <dankavatha01@gmail.com>
2024-09-10 20:47:57 +0300

    handled debug mode for production

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uhf6=c8n2(h#)*tvc!w%02##*$kx!r4c62q30$&x6uagd1xuuz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DJANGO_DEBUG')).lower() == 'true'
+
+print ("DEBUG", DEBUG, type(DEBUG))
+
+
 
 ALLOWED_HOSTS = [
     ".railway.app" #https://saas.prod.railway.app
@@ -43,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+#apps installed
     'visits',
 ]
 
