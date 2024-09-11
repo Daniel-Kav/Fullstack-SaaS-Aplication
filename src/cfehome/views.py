d8930b1c278d64ac7665077dd7e5906a00af6f89
daniel-kav <dankavatha01@gmail.com>
2024-09-11 09:37:02 +0300

    configured a postgress database

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -3,5 +3,5 @@ from visits.models import PageVisit
 
 def home(request):
     queryset = PageVisit.objects.all()
-    PageVisit.objects.create()
+    PageVisit.objects.create(path = request.path)
     return render(request, 'home.html',{'queryset': queryset.count()})
