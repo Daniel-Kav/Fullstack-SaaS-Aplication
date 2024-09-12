b727331a11dab505fcb1b63a66ef2ae131ada6cf
daniel-kav <dankavatha01@gmail.com>
2024-09-12 17:32:36 +0300

    registration view

--- src/auth/views.py
+++ src/auth/views.py
@@ -15,4 +15,14 @@ def login_view(request):
             return redirect("/")
     return render(request,'auth/login.html',{})
 
-def 
+def register_user(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username= password, password=password)
+
+        if request.user:
+            user.save()
+            login(request, user)
+            return redirect("/")
+    return render(request,'auth/register.html',{})
