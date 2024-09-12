bcb2b3c65946894410a3814ed1cb27e2256d8f18
daniel-kav <dankavatha01@gmail.com>
2024-09-12 17:00:04 +0300

    login view

--- src/auth/views.py
+++ src/auth/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login,authenticate
 
 # Create your views here.
+def login_view(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(username= username, password= password)
+
+
+    if request.user is not None:
+        login(request, user)
+        print ("Login successful")
+        redirect("/")
+    return render(request,'auth/login.html',{})
